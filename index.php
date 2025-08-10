232fa66b88e1b0f36ad7d623ec51217f084bfdc6
JKaye55 <julliannekayefernando@gmail.com>
2025-08-10 20:21:07 +0800

    TUTORIAL 10

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$name =$address =$email = "";
-$nameErr= $addressErr= $emailErr ="";
+
+ include("connections.php");
+
+$name =$address =$email =$password =$cpassword= "";
+$nameErr= $addressErr= $emailErr =$passwordErr =$cpasswordErr="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -22,6 +25,40 @@ if (empty($_POST["email"])){
 }else{
 		$email = $_POST["email"];
 	}
+if (empty($_POST["password"])){
+	$passwordErr= "Password is required!";
+
+}else{
+		$password = $_POST["password"];
+	}
+if (empty($_POST["cpassword"])){
+	$cpasswordErr= "Confirm Password is required!";
+
+}else{
+		$cpassword = $_POST["cpassword"];
+	}
+
+
+if ($name && $address && $email && $password && $cpassword){
+
+$check_email = mysqli_query($connections, "SELECT * FROM mytbl WHERE email='$email'");
+$check_email_row= mysqli_num_rows($check_email);
+
+if($check_email_row > 0){
+$emailErr="Email is already registered!";
+}else{
+
+	echo "pasok";
+
+
+}
+
+$query= mysqli_query($connections, "INSERT INTO mytbl (name,address,email, password, account_type) VALUES ('$name', '$address', '$email', '$cpassword', '2')");
+echo "<script language='javascript'>alert('New Record has been inserted!')</script>";
+echo "<script>window.location.href='index.php';</script>";
+
+}
+
 }
 ?>
 
@@ -47,15 +84,21 @@ include("nav.php");
 <form method="POST" action="<?php htmlspecialchars("PHP_SELF"); ?>">
 
 
-<input type= "text" name="name" value="<?php echo $name; ?>"> <br>
+Name: <input type= "text" name="name" value="<?php echo $name; ?>"> <br>
 <span class="error"><?php echo $nameErr; ?></span> <br>
 
-<input type= "text" name="address" value="<?php echo $address; ?>"> <br>
+Address: <input type= "text" name="address" value="<?php echo $address; ?>"> <br>
 <span class="error"><?php echo $addressErr; ?></span> <br>
 
 
-<input type= "text" name="email" value="<?php echo $email; ?>"> <br>
+Email: <input type= "text" name="email" value="<?php echo $email; ?>"> <br>
 <span class="error"><?php echo $emailErr; ?></span> <br>
+
+Password: <input type= "password" name="password" value="<?php echo $password; ?>"> <br>
+<span class="error"><?php echo $passwordErr; ?></span> <br>
+
+Confirm Password: <input type= "password" name="cpassword" value="<?php echo $cpassword; ?>"> <br>
+<span class="error"><?php echo $cpasswordErr; ?></span> <br>
 
 
 <input type= "submit" value="Submit">
@@ -65,12 +108,6 @@ include("nav.php");
 
 <hr>
 <?php
- include("connections.php");
-if ($name && $address && $email){
- $query= mysqli_query($connections, "INSERT INTO mytbl (name,address,email) VALUES ('$name', '$address', '$email')");
- echo "<script language='javascript'>alert('New Record has been inserted!')</script>";
- echo "<script>window.location.href='index.php';</script>";
-}
 
 $view_query = mysqli_query($connections, "SELECT * FROM mytbl");
 
